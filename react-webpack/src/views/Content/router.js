10aac1ceb63963fd4d926a430afc10223dc72a25
zhangyi <1090362846@qq.com>
2018-04-21 21:48:02 +0800

    refactor: delete log

--- react-webpack/src/views/Content/router.js
+++ react-webpack/src/views/Content/router.js
@@ -4,8 +4,6 @@ const Home = asyncComponent(() => import('./Home'))
 const About = asyncComponent(() => import('./About'))
 const Topics = asyncComponent(() => import('./Topics'))
 
-console.log(import('./About'), 'this is about')
-
 const routes = [
   {
     path: '/',
