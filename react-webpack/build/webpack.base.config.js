b3ad0801ca807044e778eed11a8f514618519abe
zhangyi <1090362846@qq.com>
2018-05-02 15:30:09 +0800

    feat: change output.publicPath and devServer publicPath

--- react-webpack/build/webpack.base.config.js
+++ react-webpack/build/webpack.base.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    publicPath: './',
+    publicPath: '/',
     filename: '[name].js',
     chunkFilename: 'chunk/[name].[chunkhash].js'
   },
