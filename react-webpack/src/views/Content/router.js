0f3c59ee4f46c08b8bb2aef788bfefa4afb83fdd
zhangyi <1090362846@qq.com>
2018-05-04 10:00:22 +0800

    feat: remove hot loader and use webpack HMR

--- react-webpack/src/views/Content/router.js
+++ react-webpack/src/views/Content/router.js
@@ -1,14 +1,8 @@
-// import asyncComponent from '../../commons/AsyncComponent'
+import asyncComponent from '../../commons/AsyncComponent'
 
-// const Home = asyncComponent(() => import('./Home'))
-// const About = asyncComponent(() => import('./About'))
-// const Topics = asyncComponent(() => import('./Topics'))
-
-import loadable from 'loadable-components'
-
-export const Home = loadable(() => import('./Home'))
-export const About = loadable(() => import('./About'))
-export const Topics = loadable(() => import('./Topics'))
+const Home = asyncComponent(() => import('./Home'))
+const About = asyncComponent(() => import('./About'))
+const Topics = asyncComponent(() => import('./Topics'))
 
 const routes = [
   {
