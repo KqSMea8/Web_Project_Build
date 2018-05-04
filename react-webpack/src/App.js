9dd6b15bb85220eab5146e741ee8a3695a128cf2
zhangyi <1090362846@qq.com>
2018-05-04 10:03:37 +0800

    refactor: remove not use code

--- react-webpack/src/App.js
+++ react-webpack/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
-import './styles.scss'
 
+import './styles.scss'
 import Button from './components/Button'
 import Input from './components/Input'
 import StyleComponent from './components/StyleComponent'
-
 import RouterLink from './views/RouterLink'
 
 class App extends Component {
@@ -43,5 +42,4 @@ class App extends Component {
   }
 }
 
-// export default App
 export default hot(module)(App)
--- react-webpack/src/main.js
+++ react-webpack/src/main.js
@@ -1,9 +1,7 @@
 import React from 'react'
-// import ReactDOM, { render } from 'react-dom'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux'
-// import { AppContainer } from 'react-hot-loader'
 
 import App from './App'
 import store from './redux/store'
@@ -16,24 +14,3 @@ render((
     </ConnectedRouter>
   </Provider>
 ), document.getElementById('app'))
-
-// render(
-//   <AppContainer>
-//     <Provider store={store}>
-//       <ConnectedRouter history={history}>
-//         <App />
-//       </ConnectedRouter>
-//     </Provider>
-//   </AppContainer>,
-//   document.getElementById('app')
-// )
-
-// // Hot Module Replacement API
-// if (module.hot) {
-//   module.hot.accept(App, () => {
-//     // if you are using harmony modules ({modules:false})
-//     render(App)
-//     // in all other cases - re-require App manually
-//     // render(require('./containers/App'))
-//   })
-// }
