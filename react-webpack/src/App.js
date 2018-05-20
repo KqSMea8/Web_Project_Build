377f545239f4cc8fd16905a049b9ad68b03d7899
zhangyi <1090362846@qq.com>
2018-05-20 16:56:11 +0800

    refactor: refactor code

--- react-webpack/src/App.js
+++ react-webpack/src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
+import {
+  HashRouter as Router // HashRouter / BrowserRouter
+} from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
 
 import 'normalize.css'
 import './styles.scss'
 import RouterLink from './views/RouterLink'
+import ContnetRouter from './views/Content/router'
+
+// 合并路由
+const routes = [
+  ...ContnetRouter
+]
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <RouterLink />
-      </div>
+      <Router>
+        <React.Fragment>
+          <RouterLink />
+          {renderRoutes(routes)}
+        </React.Fragment>
+      </Router>
     )
   }
 }
