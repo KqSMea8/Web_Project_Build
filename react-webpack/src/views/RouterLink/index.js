e95387423204ceb64b2e3b39452b5e9c4008650a
zhangyi <1090362846@qq.com>
2018-06-27 18:57:46 +0800

    feat: add ui

--- react-webpack/src/views/RouterLink/index.js
+++ react-webpack/src/views/RouterLink/index.js
@@ -23,19 +23,25 @@ class RouterLink extends Component {
         <div className="silder-children">
           <div className="logo">
             <a href="/">
-              <span>◢ ◤</span>
+              <img src="src/assets/logo.png" alt="logo" />
               <h1>Singularity</h1>
             </a>
           </div>
           <ul className="menu">
-            <li>
-              <Link to="/">首页</Link>
+            <li className="menu-item">
+              <div className="meun-item-title">
+                <Link to="/">首页</Link>
+              </div>
             </li>
-            <li>
-              <Link to="/about">关于</Link>
+            <li className="menu-item">
+              <div className="meun-item-title">
+                <Link to="/about">关于</Link>
+              </div>
             </li>
-            <li>
-              <Link to="/topics">主题列表</Link>
+            <li className="menu-item">
+              <div className="meun-item-title">
+                <Link to="/topics">主题列表</Link>
+              </div>
             </li>
           </ul>
         </div>
