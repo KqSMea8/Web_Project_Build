de32cec6fe515c3b2347c2e8e7906a377cf98f58
zhangyi <1090362846@qq.com>
2018-09-18 14:59:09 +0800

    update

--- react-webpack/src/views/Content/Home/index.js
+++ react-webpack/src/views/Content/Home/index.js
@@ -7,6 +7,13 @@ import { push } from "connected-react-router";
 import { setDemoData } from "../../../redux/actions/demo";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -18,6 +25,18 @@ class Home extends Component {
         <br />
         <button onClick={() => this.props.dispatch(push("/about"))}>
           arrive about page
+        </button>
+        <br />
+        <br />
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={e => this.setState({ value: e.target.value })}
+        />
+        <br />
+        <br />
+        <button onClick={() => this.setState({ value: "重制value" })}>
+          重制value
         </button>
         {this.props.demoData.map(item => {
           return <div key={item.id}>{item.name}</div>;
